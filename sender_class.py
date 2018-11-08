62d883e888c919ed37b7e5bb42134f1e9c272994
petersjanjaap <43883509+petersjanjaap@users.noreply.github.com>
2018-11-08 15:43:45 +0100

    'classes'

---
+++ sender_class.py
@@ -0,0 +1,13 @@
+"""
+generates a class for province
+elements included are:
+- sender type (ranging from A to G)
+- sender costs
+"""
+
+class sender(object):
+
+    # sender objects consist of type and costs
+    def __init__(self, type, costs):
+        self.type = type
+        self.costs = costs
