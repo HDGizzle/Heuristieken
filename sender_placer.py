7be11a78f2d58bfe89528098afb631a399f65267
petersjanjaap <43883509+petersjanjaap@users.noreply.github.com>
2018-11-09 12:27:45 +0100

    sender placing file

--- sender_placer.py
+++ sender_placer.py
@@ -12,7 +12,7 @@ from sender_class import Sender
 # define global variables
 SENDERS = {}
 PROVINCES = {}
-INPUT_CSV = "nederland.csv"
+INPUT_CSV = "usa_borders.csv"
 
 def sender_initialiser():
     """
@@ -34,21 +34,39 @@ def province_initialiser():
     initializes all province objects and adds them global dictionary
     provinces are imported from csv files
     """
+
+    # opens csv file containing province data
     with open(INPUT_CSV) as myfile:
         datafile = pandas.read_csv(myfile)
-        # iterates over row and saves data to global dictionary
-        print(datafile.dtypes)
-        print(datafile.index)
-        print(datafile.values)
-        print(datafile.columns)
-        print(datafile.describe())
-        print(datafile)
 
+        # iterates over rows in csv file
+        for i in range(len(datafile)):
 
+            # extracts province name and neighbors as a list
+            name = datafile["name"][i]
+            neighbors = str(datafile["neighbors"][i]).split(", ")
+
+            # replaces pandas designation 'nan' for empty with empty list
+            if neighbors[0] in "nan":
+                neighbors = []
+
+            # adds new province object to PROVINCES
+            PROVINCES[name] = Province(name, neighbors)
+
+def welsh_powell():
+    """
+    this function uses the Welsh Powell algoritm to find a solution for the
+    placing of senders in provinces under color graphing constraints
+    """
 
 if __name__ == "__main__":
+    """
+    loads provinces and senders and finds optimal solution using algoritm
+    """
     # initializes all senders and adds them to SENDERS
     sender_initialiser()
 
     # initializes all province objects and adds them to PROVINCES
     province_initialiser()
+
+    sender
