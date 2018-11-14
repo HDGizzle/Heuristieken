7d06e6a0c1fb3099537317c84f11276eb9617803
petersjanjaap <43883509+petersjanjaap@users.noreply.github.com>
2018-11-14 12:37:42 +0100

    welsh powell in progress

--- sender_placer.py
+++ sender_placer.py
@@ -5,14 +5,14 @@ bordering provinces cannot have the same sender type
 
 # imports libraries
 import pandas
-import csv
 from province_class import Province
 from sender_class import Sender
 
 # define global variables
 SENDERS = {}
 PROVINCES = {}
-INPUT_CSV = "usa_borders.csv"
+INPUT_CSV = "russia_borders.csv"
+
 
 def sender_initialiser():
     """
@@ -20,7 +20,7 @@ def sender_initialiser():
     """
 
     # sender types range from A-G
-    sender_types = ["ABCDEFG"]
+    sender_types = [1, 2, 3, 4, 5, 6, 7, 8]
 
     # for first project sender costs are initialised to zero
     costs = 0
@@ -28,6 +28,7 @@ def sender_initialiser():
     # adds all different sender type objects to SENDERS
     for char in sender_types:
         SENDERS[char] = Sender(char, costs)
+
 
 def province_initialiser():
     """
@@ -43,7 +44,7 @@ def province_initialiser():
         for i in range(len(datafile)):
 
             # extracts province name and neighbors as a list
-            name = datafile["name"][i]
+            name = datafile["name"][i].strip()
             neighbors = str(datafile["neighbors"][i]).split(", ")
 
             # replaces pandas designation 'nan' for empty with empty list
@@ -52,6 +53,7 @@ def province_initialiser():
 
             # adds new province object to PROVINCES
             PROVINCES[name] = Province(name, neighbors)
+
 
 def neighbor_sorted_provinces():
     """
@@ -85,12 +87,55 @@ def neighbor_sorted_provinces():
     return connections_sort
 
 
-def welsh_powell():
+def welsh_powell(province_pools):
     """
     this function uses the Welsh Powell algoritm to find a solution for the
     placing of senders in provinces under color graphing constraints
     http://mrsleblancsmath.pbworks.com/w/file/fetch/46119304/vertex%20coloring%20algorithm.pdf
     """
+
+    # lists all numbers of connections from high to low
+    connections = sorted(province_pools, reverse=True)
+
+    # iterates over senders
+    for sender in SENDERS:
+        # iterates over province pools
+        for connection in connections:
+
+            # iterates over provinces in province pools
+            province_pool = province_pools[connection]
+            for province_name in province_pool:
+
+                # extracts province object
+                province = PROVINCES[province_name]
+
+                # check for presence of sender in neighbor
+                present = 0
+
+                # iterate over neighbors
+                for neighbor in province.neighbors:
+
+                    # check if sender neighbor corresponds to current sender
+                    if PROVINCES[neighbor].sender == sender:
+                        present = 1
+
+                # place sender if sender is not in neighbors
+                if present == 0:
+
+                    # place sender
+                    province.sender = sender
+
+                    # remove province from pool
+                    province_pools[connection].remove(province_name)
+
+                    print(sender)
+
+                    # check if pool still contains provinces
+                    if len(province_pools[connection]) == 0:
+
+                        # if not skip pool
+                        connections.remove(connection)
+
 
 if __name__ == "__main__":
     """
@@ -104,7 +149,22 @@ if __name__ == "__main__":
     province_initialiser()
 
     # returns pools of provinces based on number of neighbors into dictionary
-    neighbor_sorted_provinces()
+    sorted_provs = neighbor_sorted_provinces()
+
+    welsh_powell(sorted_provs)
 
     # works out sender solutions
     # welsh_powell()
+    for province in PROVINCES:
+        if not (PROVINCES[province].sender):
+            print('NEEN')
+            print(PROVINCES[province].name)
+        for neighbor in PROVINCES[province].neighbors:
+            if PROVINCES[neighbor].sender == PROVINCES[province].sender:
+                print('WERKT NIET')
+                print(f'{PROVINCES[neighbor].name}, {PROVINCES[province].name},\
+                {PROVINCES[province].sender}')
+
+        # print(PROVINCES[province].name)
+    #     print(PROVINCES[province].sender)
+    #     # print(f'//PROVINCES[province]//')
