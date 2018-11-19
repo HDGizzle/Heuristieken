93531bedc94fd360024e25ac01d032987b9a25d8
petersjanjaap <43883509+petersjanjaap@users.noreply.github.com>
2018-11-19 15:42:17 +0100

    sender placer bijgewrkt

--- sender_placer.py
+++ sender_placer.py
@@ -132,8 +132,116 @@ def welsh_powell(province_pools):
                     province.sender = sender
                     senders_needed = sender
 
-    print(senders_needed)
+    if senders_needed < 4:
+        print(senders_needed)
 
+
+def clusters():
+    """
+    calculates the minimum amount of senders need to fill the map based on an
+    assessment of the interconnectedness of all neighbors, i.e. clusters
+    """
+
+    # dictionary to keep track of all cluster pools based on length
+    clusters = {}
+
+    max_interconnections = 0
+
+    # iterate over province objects
+    for province in PROVINCES:
+
+        # iterate over all neighbors of given province
+        for neighbor in PROVINCES[province].neighbors:
+            cluster_participants = [PROVINCES[province].name, neighbor]
+
+            # connections is by default 1 for a neighbor
+            interconnections = 1
+
+            # iterate over neighbors of the selected neighbor
+            for other_neighbor in PROVINCES[province].neighbors:
+
+                # check if neighbor is in neighbor of in neighbors
+                if other_neighbor in PROVINCES[neighbor].neighbors:
+
+                    # add neighbor to given cluster
+                    cluster_participants.append(other_neighbor)
+                    interconnections += 1
+
+            if interconnections in clusters:
+                clusters[interconnections].append(cluster_participants)
+
+            else:
+                clusters[interconnections] = cluster_participants
+
+            # keep track of max interconnections measured
+            if interconnections > max_interconnections:
+                max_interconnections = interconnections
+
+    # return the dictionary containing all clusters per province
+    for pool in clusters:
+        for i in range(len(clusters[pool])):
+            if isinstance(clusters[pool][i], str):
+                clusters[pool][i] = [clusters[pool][i]]
+
+    return(clusters, max_interconnections)
+
+
+def sender_placer(name, provinces_test, senders_available):
+    """
+    places senders in a province
+    """
+    # check what senders are usable
+    for sender in senders_available:
+        usable = True
+
+        # checks senders type for other neighbors
+        for neighbor in provinces_test[name].neighbors:
+
+            # if sender is with neighbor, sender cannot be used
+            if sender == provinces_test[neighbor].sender:
+                usable = False
+
+        # places sender if usable
+        if usable:
+            provinces_test[name].sender = sender
+
+def greedy_cluster(clusters, current_cluster, provinces_test, senders_available):
+
+    # iterate over names in cluster
+    for province in current_cluster:
+
+        # check if sender is already placed
+        if not provinces_test[province].sender:
+            sender_placer(provinces_test[province].name, provinces_test, senders_available)
+
+    for pool in clusters:
+        for i in range(len(clusters[pool])):
+            if current_cluster == clusters[pool][i]:
+                clusters[pool][i] == []
+
+    # iterate over names in cluster
+    for province in current_cluster:
+        for neighbor in provinces_test[province].neighbors:
+            if not provinces_test[neighbor].sender and neighbor not in current_cluster:
+                for pool in clusters:
+                    for cluster in clusters[pool]:
+                        if neighbor in cluster and cluster != current_cluster:
+                            greedy_cluster(clusters, cluster, provinces_test, senders_available)
+
+
+    # iterate over all provinces and clusters it participates in
+
+# def recursive_placer(province_name, provinces_test, visited_provinces, senders_available):
+
+    # # add provinces to visited provinces
+    # visited_provinces.append(province_name)
+    #
+    # sender_placer(province_name, provinces_test, senders_available)
+    #
+    # # repeat recursion for unvisited provinces
+    # for neighbor in provinces_test[province_name].neighbors:
+    #     if neighbor not in visited_provinces:
+    #         sender_placer(neighbor, provinces_test, visited_provinces)
 
 if __name__ == "__main__":
     """
@@ -173,6 +281,7 @@ if __name__ == "__main__":
                 # position of each name is moved by one every iteration of i
                 order[j] = province_pools[pool][(j + i) % (len_list)]
 
+            print(order)
             # add newly ordered pool to dictionary
             pool_sorts[pool].append(order)
 
@@ -184,9 +293,8 @@ if __name__ == "__main__":
     for pool in province_pools:
         list_counter.append(len(province_pools[pool]) - 1)
 
-    # keeps track of
+    # keeps track of all variations
     variation = 0
-    y = 0
 
     # makes all possible combines of available sorts in pools
     while list_counter[-1] >= list_cursor[-1]:
@@ -203,12 +311,60 @@ if __name__ == "__main__":
         # iterates over amount of pools
         for i in range(1, len(province_pools)):
 
-            # if the cursor has finished all variations for a given pool, the
-            # cursor for the past pool is reset to zero and starts cursing in
-            # the new pool
+            """
+            if the cursor has finished all variations for a given pool, the
+            cursor for the past pool is reset to zero and starts cursing in
+            the new pool
+            """
             if list_counter[i - 1] < list_cursor[i - 1]:
                 list_cursor[i] += 1
                 list_cursor[i - 1] = 0
 
         # execute wellsh powel for the given variation
-        welsh_powell(dictionary)
+        # welsh_powell(dictionary)
+    #
+    list = clusters()
+    clusters = list[0]
+    max_interconnections = list[1]
+
+
+    # use a copy of all provinces
+    provinces_test = copy.deepcopy(PROVINCES)
+    #
+    # keeps track of provinces visited through algoritm
+    visited_provinces = []
+
+    # senders to use is based on minimum amount needed
+    senders_available = []
+
+    for i in range(1, (max_interconnections + 1)):
+        senders_available.append(i)
+
+    for pool in clusters:
+        for cluster in clusters[pool]:
+            greedy_cluster(clusters, cluster, provinces_test, senders_available)
+
+    # # fills all provinces without neighbors
+    # for province in provinces_test:
+    #
+    #     # checks if provinces has no neighbors
+    #     if not provinces_test[province].neighbors:
+    #
+    #         # places sender
+    #         provinces_test[province].sender = senders_available[0]
+    #
+    #         # adds provinces to visited provinces
+    #         visited_provinces.append(province)
+    #
+    # # fills a map based on the minimum amount of senders needed
+    # for province in provinces_test:
+    #     sender_placer(province, provinces_test, senders_available)
+    # #
+    for province in provinces_test:
+        # print(province)
+        if not provinces_test[province].sender:
+            print(f'{province} geen zender')
+        for neighbor in provinces_test[province].neighbors:
+            if provinces_test[province].sender == provinces_test[neighbor].sender:
+                if provinces_test[province].sender:
+                    print('overlap')
