176ed3da827c0c21bfd0c867fff91013224b256a
petersjanjaap <43883509+petersjanjaap@users.noreply.github.com>
2018-11-28 11:34:58 +0100

    ordered files

---
+++ main/objects/province_class.py
@@ -0,0 +1,47 @@
+"""
+generates a class for province
+elements included are:
+- province name
+- a list of neighbors names
+- a sender object
+- sender options (for state space calculation)
+"""
+
+
+class Province():
+    """"
+    province objects consist of name, neighbors list, current sender and
+    senders placeable. sender placed and sender options are initialised to None
+    """
+    def __init__(self, name, neighbors):
+        self.name = name
+        self.neighbors = neighbors
+        self.sender = None
+        self.sender_options = None
+
+    def sender_placer(self, provinces, senders):
+        """
+        places senders in a province
+        """
+
+        if not self.sender:
+            # check what senders are usable
+            for sender in senders:
+
+                # defines whether sender is usable
+                usable = True
+
+                # checks senders type for other neighbors
+                for neighbor in self.neighbors:
+
+                    # if sender is with neighbor, sender cannot be used
+                    if sender == provinces[neighbor].sender:
+                        usable = False
+
+                # places sender if usable
+                if usable:
+                    self.sender = sender
+                    if sender > 3:
+                        print(self.name)
+                        print(sender)
+                    break
---
+++ main/sender_placer.py
@@ -0,0 +1,37 @@
+"""
+this script places senders in different provinces under the constraint that
+bordering provinces cannot have the same sender type
+"""
+
+# imports files and functions
+import os
+import sys
+basepath = os.path.abspath(os.path.curdir).split("Heuristieken")[0] + "Heuristieken"
+sys.path.append(os.path.join(basepath, r"algorithms\greedy"))
+sys.path.append(os.path.join(basepath, r"algorithms\state_space"))
+sys.path.append(os.path.join(basepath, "initialiser"))
+from welsh_powell import welsh_powell_variation
+import initialiser as init
+import state_space as state
+
+# define global variables
+INPUT_CSV = 'nederland.csv'
+PROVINCES = init.province_initialiser(INPUT_CSV)
+SENDERS = init.sender_initialiser()
+
+
+if __name__ == "__main__":
+    """
+    loads provinces and senders and finds solutions using named
+    """
+
+    # returns pools of provinces based on number of neighbors into dictionary
+    province_pools = init.neighbor_sorted_provinces(PROVINCES)
+
+    # calculates the state space for a given map
+    state.cluster_partipication(PROVINCES, SENDERS, province_pools)
+    # lower_bound(PROVINCES, SENDERS)
+    # state_space(PROVINCES, SENDERS)
+
+    # uses the wellsh powel function to test different variations of pool sorts
+    welsh_powell_variation(province_pools, PROVINCES, SENDERS)
