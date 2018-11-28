80f839c97a6b9086b7751d9f88e3a1d57716f629
Amber <ambernobel93@gmail.com>
2018-11-28 16:06:34 +0100

    this function calculates the money spent on the senders

---
+++ main/checker.py
@@ -0,0 +1,38 @@
+"""
+this file provides functions to see if the senders are placed correctly and
+calculates the total amount of money used for the solution
+"""
+
+
+def sender_checker(provinces):
+    """
+    checks if outcome is valid by checking senders on constraints
+    """
+    # determines validity of outcome
+    valid = True
+    # iterate over provinces
+    for province in provinces:
+
+        # check if neighbor has same sendertype and senders are placed
+        for neighbor in provinces[province].neighbors:
+            if provinces[neighbor].sender == provinces[province].sender:
+                valid = False
+
+            elif not provinces[neighbor].sender:
+                valid = False
+
+    # returns the outcome of the check
+    return valid
+
+
+def cost_calculator(provinces):
+    """
+    calculates the total costs based on cost of senders used
+    """
+
+    costs = 0
+
+    for province in provinces:
+        costs += provinces[province].sender.costs
+
+    return costs
