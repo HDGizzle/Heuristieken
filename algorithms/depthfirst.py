0af3b7378bf7a94ab83339e09144ee6f2aab5e14
HDGizzle <44022533+HDGizzle@users.noreply.github.com>
2018-11-29 17:42:04 +0100

    Update depthfirst.py

--- algorithms/depthfirst.py
+++ algorithms/depthfirst.py
@@ -4,18 +4,25 @@ import math
 
 
 # define province dictionary and different senders
-provinces = init.province_initialiser("nederland.csv")
+provinces = init.province_initialiser("usa_borders.csv")
 sendtypes = ["A", "B", "C", "D", "E", "F", "G"]
 
 
 visited = [] # blijft intact
-stack = ["Groningen"] # wordt gepopt, staat een beginprovince in
-print(provinces["Friesland"].sender)
-print(provinces[stack[-1]].sender)
-print(len(provinces))
+stack = ["Wyoming"] # wordt gepopt, staat een beginprovince in
+# print(len(provinces["Utrecht"].neighbors))
+used = []
+
+# print(provinces[stack[-1]].sender)
+# print(len(provinces))
+
+for province in provinces:
+    if len(provinces[province].neighbors) == 0:
+        provinces[province].sender = "A"
+        visited.append(province)
 
 while len(visited) != len(provinces):  # zolang er unvisited provinces zijn
-    temp = sendtypes
+    temp = ["A", "B", "C", "D", "E", "F", "G"]
 
     if provinces[stack[-1]].sender == None:       # mocht je geen stap terug hebben genomen wil je deze loop in gaan
 
@@ -28,6 +35,8 @@ while len(visited) != len(provinces):  # zolang er unvisited provinces zijn
                 # print(sendtype)
                 temp.remove(provinces[neighbor].sender)
             provinces[stack[-1]].sender = temp[0]  # gebruik eerste zender uit de lijst met ongebruikte zendtypes om minimaal aantal verschillende zenders te gebruiken
+            if temp[0] not in used:
+                used.append(temp[0])
 
         count = 0
         for neighbor in provinces[stack[-1]].neighbors:
@@ -43,7 +52,7 @@ while len(visited) != len(provinces):  # zolang er unvisited provinces zijn
                 if neighbor not in visited:
                     list.append(neighbor)
 
-            stack.append(list[0]) # begint bovenaan while loop en dit wordt de nieuwe stack[-1]
+            stack.append(random.choice(list)) # begint bovenaan while loop en dit wordt de nieuwe stack[-1]
 
 
     else:                           # als je wel een stap terug hebt genomen doe je dit
@@ -61,8 +70,9 @@ while len(visited) != len(provinces):  # zolang er unvisited provinces zijn
                 if neighbor not in visited:
                     list.append(neighbor)
 
-            stack.append(list[0]) # begint bovenaan while loop en dit wordt de nieuwe stack[-1]
+            stack.append(random.choice(list)) # begint bovenaan while loop en dit wordt de nieuwe stack[-1]
 
 for province in provinces:
 
     print(provinces[province].sender, province)
+print(used)
