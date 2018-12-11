896546727e790b8d99651ea0a6e638259245fd29
petersjanjaap <43883509+petersjanjaap@users.noreply.github.com>
2018-12-11 13:59:48 +0100

    hillclimber update

--- algorithms/hillclimber/hillclimber.py
+++ algorithms/hillclimber/hillclimber.py
@@ -6,6 +6,7 @@ or cash used
 # libraries to import
 import os
 import sys
+import random
 
 #  import paths
 basepath = os.path.abspath(os.path.curdir).split("Heuristieken")[0] + \
@@ -18,69 +19,67 @@ import checker as check
 
 def hill_climber(provinces, senders):
     """
-    this function first places a sender in every chart and then makes alterations
+    this function first places a sender in chart and then makes alterations
     in the hope of improvement
     """
 
-    check.place_sender(provinces, senders)
+    check.place_senders(provinces, senders)
 
     outcome = check.save_outcome(provinces)
-
+    print(check.total_costs(provinces, outcome))
     # amount of changes maximally to be made is set at limit
-    change = 0
-    limit = 900
+    limit = 10
 
     # outcome is the state
-    outcome = alteration(provinces, senders, outcome, change, limit)
+    outcome = alteration(provinces, senders, outcome, limit)
+    print(check.total_costs(provinces, outcome))
 
-
-def alteration(provinces, senders, outcome, changes, limit):
+def alteration(provinces, senders, outcome, limit):
     """
     This function tries to improve the current outcome by implementing random
     alterations
     """
 
-    # list is used to put senders in provinces
-    provinces_list = list(provinces.keys())
+    types = check.types_used(outcome)
 
-    alteration_made = False
-    if changes < limit:
+    # hillclimber can only make maximum amount of alterations
+    if limit > 0:
 
-        for sender_type in senders:
+        random.seed(limit)
+
+        # list is used to put senders in provinces and is shuffled every time
+        provinces_list = list(provinces.keys())
+        random.shuffle(provinces_list)
+
+        #  iterate over senders
+        for type in types:
             for province in provinces_list:
+
+                #  check if sender can be placed
                 new_sender = True
                 for neighbor in provinces[province].neighbors:
-                    if sender_type == provinces[neighbor].sender.type:
+                    if type == provinces[neighbor].sender.type:
                         new_sender = False
 
-                # places random new senders and alteration is made
+                # places new sender if possible
                 if new_sender:
-                    alteration_made = True
 
                     # remove province from list to alterate
-                    provinces[province].sender = senders[sender_type]
+                    provinces[province].sender = senders[type]
                     provinces_list.remove(province)
 
-        # if no alteration is made the highest sender types are removed
-        if not alteration_made:
-            highest_sender = check.types_used(provinces)[0]
-
-            # these are replaced by the lowest sender type
-            for province in provinces:
-                if provinces[province].sender.type == highest_sender:
-                    provinces[province].sender.type == senders[1]
-
-            changes += 1
         # checks if outcome is valid, if so compare against benchmark
         if check.validity_check(provinces):
 
+            #  save new outcome and compare against benchmark
             new_outcome = check.save_outcome(provinces)
-
-            if check.enhanced_distribution(new_outcome, outcome):
+            if check.total_costs(provinces, new_outcome) < check.total_costs(provinces, outcome):
                 outcome = new_outcome
-            changes += 1
-        return alteration(provinces, senders, outcome, changes, limit)
 
-    # algoritm is done and best outcome can be returned
+        limit -= 1
+        # make new alteration
+        return alteration(provinces, senders, outcome, limit)
+
+    # algoritm has made max alterations and returns best outcome
     else:
         return outcome
