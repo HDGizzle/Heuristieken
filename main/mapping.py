5db587d1e26a6ba9c8d95d80fba58764ba2af618
A-mberr <44018844+A-mberr@users.noreply.github.com>
2018-12-18 02:33:10 +0100

    Update mapping.py
    creates coloured maps based on the welsh Powell

--- main/mapping.py
+++ main/mapping.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.join(basepath, "algorithms", "constructive"))
 sys.path.append(os.path.join(basepath, "algorithms", "hillclimber"))
 sys.path.append(os.path.join(basepath, "algorithms", "state_space"))
 sys.path.append(os.path.join(basepath, "initialiser"))
+sys.path.append(os.path.join(basepath, "main", "shapefiles"))
 sys.path.append(os.path.join(basepath, "visualiser", "shapefiles"))
 import matplotlib
 if sys.platform == "darwin":
@@ -20,7 +21,10 @@ from welsh_powell import welsh_powell_variation
 from depthfirst import depth_first
 import initialiser as init
 import state_space as state
-from usa_vis import mapplotter
+from usa_vis import mapplotterUSA
+from ukr_vis import mapplotterUKR
+from russia_vis import mapplotterRUSSIA
+from china_vis import mapplotterCHINA
 
 
 # define global variables
@@ -42,17 +46,30 @@ if __name__ == "__main__":
     # lower_bound(PROVINCES, SENDERS)
     # state_space(PROVINCES, SENDERS)
 
+    # WELSH POWELL
     # uses the wellsh powel function to test different variations of pool sorts
-    welsh_powell_variation(province_pools, PROVINCES, SENDERS)
+    # the [0] refers to the the outcome when welsh powell is run based on their
+    # benchmark outcome. [1] is the outcome when focused on the
+    # benchmark variance
+    benchmark = welsh_powell_variation(province_pools, PROVINCES, SENDERS, INPUT_CSV)
+    mapplotterUKR(benchmark[0])
+    mapplotterUKR(benchmark[1])
+    mapplotterUSA(benchmark[0])
+    mapplotterUSA(benchmark[1])
+    mapplotterCHINA(benchmark[0])
+    mapplotterCHINA(benchmark[1])
+    mapplotterRUSSIA(benchmark[0])
+    mapplotterRUSSIA(benchmark[1])
     
+    # DEPTH FIRST
     # uses depth first to test the different variations in sender placing
     benchmark = depth_first(PROVINCES, SENDERS, 10000)
     # Select the maplotter of the land the you have chosen for INPUT_CSV to
     # show the colored map belonging to the data.
-    # mapplotterUKR(benchmark)
-    # mapplotterUSA(benchmark)
+    mapplotterUKR(benchmark)
+    mapplotterUSA(benchmark)
     mapplotterCHINA(benchmark)
-    # mapplotterRUSSIA(benchmark)
+    mapplotterRUSSIA(benchmark)
 
     def total_costs(provinces):
         costs = 0
